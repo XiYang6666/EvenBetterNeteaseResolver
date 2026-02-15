a7d378b32742debe296e7d274783f63abe74237c
XiYang6666 <1782356858@qq.com>
2026-02-15 11:33:47 +0800

    :broom: chore

--- src/ebnr/router/search.py
+++ src/ebnr/router/search.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
-from typing import Optional
 
-from fastapi import APIRouter, Body, HTTPException
+from fastapi import APIRouter, Body
 
 from ebnr.core.types import SongInfo
 from ebnr.services.cached_api.song import search
@@ -11,15 +10,14 @@ router = APIRouter(prefix="/search", tags=["搜索"])
 
 @router.get("")
 @router.head("", include_in_schema=False)
-async def search_get(keyword: Optional[str] = None, limit: int = 10) -> list[SongInfo]:
+async def search_get(keyword: str, limit: int = 10) -> list[SongInfo]:
     """
     ## 搜索歌曲
 
     keyword 必须传入, limit 为可选项.
     返回 `SongInfo[]`.
     """
-    if not keyword:
-        raise HTTPException(400, "Invalid Request")
+
     return await search(keyword, limit)
 
 
