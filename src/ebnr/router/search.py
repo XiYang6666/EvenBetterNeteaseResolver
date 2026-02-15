3b7f6e83906908f423db40d604f0dd6970df7083
Falsw-star <Falsestar114514@outlook.com>
2026-02-15 10:45:32 +0800

    🐛 fix search docs

--- src/ebnr/router/search.py
+++ src/ebnr/router/search.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from dataclasses import dataclass
 from fastapi import APIRouter, Body, HTTPException
 
@@ -10,7 +12,7 @@ router = APIRouter(prefix="/search", tags=["歌曲搜索"])
 @router.get("")
 @router.head("", include_in_schema=False)
 async def search_get(
-    keyword: str = "",
+    keyword: Optional[str] = None,
     limit: int = 10
 ) -> list[SongInfo]:
     """
@@ -25,14 +27,18 @@ async def search_get(
 
 @dataclass
 class PostSearch:
-    keyword: str = ""
+    keyword: Optional[str] = None
     limit: int = 10
-
-    def __post_init__(self):
-        if  not self.keyword:
-            raise ValueError("Invalid Request Data")
 
 
 @router.post("")
 async def search_post(body: PostSearch = Body(...)) -> list[SongInfo]:
+    """
+    ## 搜索歌曲
+
+    keyword 必须传入, limit 为可选项.
+    成功时返回 `SongInfo[]`, 无法获取歌单时返回错误码 404.
+    """
+    if  not body.keyword:
+        raise ValueError("Invalid Request Data")
     return await search(body.keyword, body.limit)
